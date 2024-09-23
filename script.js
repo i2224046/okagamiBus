c18c5c4c20d57f12845cae506e1e4ba47e2198f4
i2224046 <130639812+i2224046@users.noreply.github.com>
2024-09-23 15:08:27 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function parseCSV(data) {
         const cols = row.split(',');
         const hour = cols[0].replace('h', '').trim();
         const minutes = cols.slice(1).map(min => {
-            const destination = min.includes('三') ? '三菱ケミカル前'
-                            : min.includes('橋') ? '奈良北団地※'
-                            : '奈良北団地';
+            const destination = min.includes('三') ? '<p class="goto"><span>三菱ケミカル前</span></p>'
+                            : min.includes('橋') ? '<p class="goto"><span>奈良北団地</span>（ことり橋経由）</p>'
+                            : '<p class="goto"><span>奈良北団地</span>（こどもの国経由）</p>';
             return { minute: parseInt(min.replace(/[三橋]/g, '').trim()), destination };
         });
         schedule.push({ hour: parseInt(hour), minutes });
@@ -63,17 +63,17 @@ function displayTimetable(buses) {
 
         // 時刻のセル
         const timeCell = document.createElement('td');
-        timeCell.innerHTML = `<div class="time">${zeroPad(bus.hour)}:${zeroPad(bus.minute)}</div>`;
+        timeCell.innerHTML = `${zeroPad(bus.hour)}:${zeroPad(bus.minute)}`;
         row.appendChild(timeCell);
 
         // 行き先のセル
         const destinationCell = document.createElement('td');
-        destinationCell.innerHTML = `<div class="goto">${bus.destination}</div>`;
+        destinationCell.innerHTML = `${bus.destination}`;
         row.appendChild(destinationCell);
 
         // あと何分かのセル
         const minutesToBusCell = document.createElement('td');
-        minutesToBusCell.innerHTML = `<div class="min">${bus.minutesToBus}分後</div>`;
+        minutesToBusCell.innerHTML = `${bus.minutesToBus}分`;
         row.appendChild(minutesToBusCell);
 
         tbody.appendChild(row);
@@ -83,7 +83,7 @@ function displayTimetable(buses) {
 // 次のバスを表示する関数
 function displayNextBus(bus) {
     const nextBusDiv = document.getElementById('next-bus');
-    nextBusDiv.innerHTML = `<p>あと${bus.minutesToBus}分で到着します。</p>`;
+    nextBusDiv.innerHTML = `あと${bus.minutesToBus}分で到着します。`;
 }
 
 // 平日か休日かを判断する関数
